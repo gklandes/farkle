9a5822ae5e5b7817aad90afe6bd0e57ae2f7f2ed
gabelandes <gabe.landes@ddifrontline.com>
2017-05-27 15:54:38 -0500

    add die component

---
+++ src/app/die/die.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-die',
+  template: `
+<img src="assets/images/{{dieImg[dieValue-1]}}" height="70" width="70" *ngIf="dieValue">
+`,
+  styleUrls: ['./die.component.scss']
+})
+export class DieComponent implements OnInit {
+
+  dieImg: object;
+  // dieValue: number;
+  dieSelect: string;
+
+  @Input() dieValue: number;
+
+  constructor() {
+    this.dieImg = [
+      'd1.svg',
+      'd2.svg',
+      'd3.svg',
+      'd4.svg',
+      'd5.svg',
+      'd6.svg'
+    ];
+    this.dieValue = 1;
+    this.dieSelect = this.dieImg[this.dieValue - 1];
+  }
+
+  ngOnInit() {
+  }
+}
