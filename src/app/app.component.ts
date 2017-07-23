13d6ef8586045393483603eb622a91fcbe9ef37a
gabelandes <gabe.landes@ddifrontline.com>
2017-07-23 13:35:04 -0500

    fix scope issue in event subscription

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,24 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
-import { Router, Event, NavigationEnd } from '@angular/router';
+    import { Component, OnInit } from '@angular/core';
+    import { Router, NavigationEnd } from '@angular/router';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
-})
-export class AppComponent implements OnInit {
-  showHeader: boolean;
+    @Component({
+      selector: 'app-root',
+      templateUrl: './app.component.html'
+    })
+    export class AppComponent implements OnInit {
+      showHeader: boolean;
 
-  constructor(
-    private router: Router,
-    private detector: ChangeDetectorRef
-  ) {}
+      constructor(
+        private router: Router
+      ) {}
 
-  ngOnInit () {
-    console.log(this.detector);
-    const d = this.detector;
-    this.showHeader = true;
-    this.router.navigate(['']);
-    this.router.events.subscribe(function (event) {
-      console.log('inside', d);
-      if (event instanceof NavigationEnd) { 
-        this.showHeader = event.url === '/';
-        d.detectChanges();
-        console.log(event.url, this.showHeader);
-      };
-    });
-  }
-
-}
+      ngOnInit () {
+        this.showHeader = false;
+        this.router.navigate(['']);
+        this.router.events.subscribe((event) => {
+          if (event instanceof NavigationEnd) {
+            this.showHeader = event.url !== '/';
+          };
+        });
+      }
+    }
